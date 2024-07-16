140079c1d03f7e709967fa413a2b3547641d374c
yokotoka <hey@yokotoka.is>
2024-07-15 23:40:53 -0300

    :bug: compat changes

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -1,12 +1,10 @@
 import jwt
 
 from django.contrib.auth import get_user_model
-from django.utils.encoding import smart_text
-from django.utils.translation import ugettext as _
+from django.utils.encoding import smart_str
+from django.utils.translation import gettext as _
 from rest_framework import exceptions
-from rest_framework.authentication import (
-    BaseAuthentication, get_authorization_header
-)
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
 
 from rest_framework_jwt.settings import api_settings
 
@@ -32,10 +30,10 @@ class BaseJSONWebTokenAuthentication(BaseAuthentication):
         try:
             payload = jwt_decode_handler(jwt_value)
         except jwt.ExpiredSignature:
-            msg = _('Signature has expired.')
+            msg = _("Signature has expired.")
             raise exceptions.AuthenticationFailed(msg)
         except jwt.DecodeError:
-            msg = _('Error decoding signature.')
+            msg = _("Error decoding signature.")
             raise exceptions.AuthenticationFailed(msg)
         except jwt.InvalidTokenError:
             raise exceptions.AuthenticationFailed()
@@ -52,17 +50,17 @@ class BaseJSONWebTokenAuthentication(BaseAuthentication):
         username = jwt_get_username_from_payload(payload)
 
         if not username:
-            msg = _('Invalid payload.')
+            msg = _("Invalid payload.")
             raise exceptions.AuthenticationFailed(msg)
 
         try:
             user = User.objects.get_by_natural_key(username)
         except User.DoesNotExist:
-            msg = _('Invalid signature.')
+            msg = _("Invalid signature.")
             raise exceptions.AuthenticationFailed(msg)
 
         if not user.is_active:
-            msg = _('User account is disabled.')
+            msg = _("User account is disabled.")
             raise exceptions.AuthenticationFailed(msg)
 
         return user
@@ -76,7 +74,8 @@ class JSONWebTokenAuthentication(BaseJSONWebTokenAuthentication):
 
         Authorization: JWT eyJhbGciOiAiSFMyNTYiLCAidHlwIj
     """
-    www_authenticate_realm = 'api'
+
+    www_authenticate_realm = "api"
 
     def get_jwt_value(self, request):
         auth = get_authorization_header(request).split()
@@ -87,15 +86,17 @@ class JSONWebTokenAuthentication(BaseJSONWebTokenAuthentication):
                 return request.COOKIES.get(api_settings.JWT_AUTH_COOKIE)
             return None
 
-        if smart_text(auth[0].lower()) != auth_header_prefix:
+        if smart_str(auth[0].lower()) != auth_header_prefix:
             return None
 
         if len(auth) == 1:
-            msg = _('Invalid Authorization header. No credentials provided.')
+            msg = _("Invalid Authorization header. No credentials provided.")
             raise exceptions.AuthenticationFailed(msg)
         elif len(auth) > 2:
-            msg = _('Invalid Authorization header. Credentials string '
-                    'should not contain spaces.')
+            msg = _(
+                "Invalid Authorization header. Credentials string "
+                "should not contain spaces."
+            )
             raise exceptions.AuthenticationFailed(msg)
 
         return auth[1]
@@ -106,4 +107,6 @@ class JSONWebTokenAuthentication(BaseJSONWebTokenAuthentication):
         header in a `401 Unauthenticated` response, or `None` if the
         authentication scheme should return `403 Permission Denied` responses.
         """
-        return '{0} realm="{1}"'.format(api_settings.JWT_AUTH_HEADER_PREFIX, self.www_authenticate_realm)
+        return '{0} realm="{1}"'.format(
+            api_settings.JWT_AUTH_HEADER_PREFIX, self.www_authenticate_realm
+        )
--- rest_framework_jwt/serializers.py
+++ rest_framework_jwt/serializers.py
@@ -4,7 +4,7 @@ from calendar import timegm
 from datetime import datetime, timedelta
 
 from django.contrib.auth import authenticate, get_user_model
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 from .compat import Serializer
 
@@ -27,6 +27,7 @@ class JSONWebTokenSerializer(Serializer):
 
     Returns a JSON Web Token that can be used to authenticate later calls.
     """
+
     def __init__(self, *args, **kwargs):
         """
         Dynamically add the USERNAME_FIELD to self.fields.
@@ -34,7 +35,7 @@ class JSONWebTokenSerializer(Serializer):
         super(JSONWebTokenSerializer, self).__init__(*args, **kwargs)
 
         self.fields[self.username_field] = serializers.CharField()
-        self.fields['password'] = PasswordField(write_only=True)
+        self.fields["password"] = PasswordField(write_only=True)
 
     @property
     def username_field(self):
@@ -43,7 +44,7 @@ class JSONWebTokenSerializer(Serializer):
     def validate(self, attrs):
         credentials = {
             self.username_field: attrs.get(self.username_field),
-            'password': attrs.get('password')
+            "password": attrs.get("password"),
         }
 
         if all(credentials.values()):
@@ -51,17 +52,14 @@ class JSONWebTokenSerializer(Serializer):
 
             if user:
                 if not user.is_active:
-                    msg = _('User account is disabled.')
+                    msg = _("User account is disabled.")
                     raise serializers.ValidationError(msg)
 
                 payload = jwt_payload_handler(user)
 
-                return {
-                    'token': jwt_encode_handler(payload),
-                    'user': user
-                }
+                return {"token": jwt_encode_handler(payload), "user": user}
             else:
-                msg = _('Unable to log in with provided credentials.')
+                msg = _("Unable to log in with provided credentials.")
                 raise serializers.ValidationError(msg)
         else:
             msg = _('Must include "{username_field}" and "password".')
@@ -73,10 +71,11 @@ class VerificationBaseSerializer(Serializer):
     """
     Abstract serializer used for verifying and refreshing JWTs.
     """
+
     token = serializers.CharField()
 
     def validate(self, attrs):
-        msg = 'Please define a validate method.'
+        msg = "Please define a validate method."
         raise NotImplementedError(msg)
 
     def _check_payload(self, token):
@@ -85,10 +84,10 @@ class VerificationBaseSerializer(Serializer):
         try:
             payload = jwt_decode_handler(token)
         except jwt.ExpiredSignature:
-            msg = _('Signature has expired.')
+            msg = _("Signature has expired.")
             raise serializers.ValidationError(msg)
         except jwt.DecodeError:
-            msg = _('Error decoding signature.')
+            msg = _("Error decoding signature.")
             raise serializers.ValidationError(msg)
 
         return payload
@@ -97,7 +96,7 @@ class VerificationBaseSerializer(Serializer):
         username = jwt_get_username_from_payload(payload)
 
         if not username:
-            msg = _('Invalid payload.')
+            msg = _("Invalid payload.")
             raise serializers.ValidationError(msg)
 
         # Make sure user exists
@@ -108,7 +107,7 @@ class VerificationBaseSerializer(Serializer):
             raise serializers.ValidationError(msg)
 
         if not user.is_active:
-            msg = _('User account is disabled.')
+            msg = _("User account is disabled.")
             raise serializers.ValidationError(msg)
 
         return user
@@ -120,15 +119,12 @@ class VerifyJSONWebTokenSerializer(VerificationBaseSerializer):
     """
 
     def validate(self, attrs):
-        token = attrs['token']
+        token = attrs["token"]
 
         payload = self._check_payload(token=token)
         user = self._check_user(payload=payload)
 
-        return {
-            'token': token,
-            'user': user
-        }
+        return {"token": token, "user": user}
 
 
 class RefreshJSONWebTokenSerializer(VerificationBaseSerializer):
@@ -137,35 +133,31 @@ class RefreshJSONWebTokenSerializer(VerificationBaseSerializer):
     """
 
     def validate(self, attrs):
-        token = attrs['token']
+        token = attrs["token"]
 
         payload = self._check_payload(token=token)
         user = self._check_user(payload=payload)
         # Get and check 'orig_iat'
-        orig_iat = payload.get('orig_iat')
+        orig_iat = payload.get("orig_iat")
 
         if orig_iat:
             # Verify expiration
             refresh_limit = api_settings.JWT_REFRESH_EXPIRATION_DELTA
 
             if isinstance(refresh_limit, timedelta):
-                refresh_limit = (refresh_limit.days * 24 * 3600 +
-                                 refresh_limit.seconds)
+                refresh_limit = refresh_limit.days * 24 * 3600 + refresh_limit.seconds
 
             expiration_timestamp = orig_iat + int(refresh_limit)
             now_timestamp = timegm(datetime.utcnow().utctimetuple())
 
             if now_timestamp > expiration_timestamp:
-                msg = _('Refresh has expired.')
+                msg = _("Refresh has expired.")
                 raise serializers.ValidationError(msg)
         else:
-            msg = _('orig_iat field is required.')
+            msg = _("orig_iat field is required.")
             raise serializers.ValidationError(msg)
 
         new_payload = jwt_payload_handler(user)
-        new_payload['orig_iat'] = orig_iat
+        new_payload["orig_iat"] = orig_iat
 
-        return {
-            'token': jwt_encode_handler(new_payload),
-            'user': user
-        }
+        return {"token": jwt_encode_handler(new_payload), "user": user}
