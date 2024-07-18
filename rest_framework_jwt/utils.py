5ab13b134989e29cefba1b01fb64c021cdd0a613
yokotoka <hey@yokotoka.is>
2024-07-18 20:08:49 -0300

    :bug: fix "str object has no attribute decode"

--- rest_framework_jwt/utils.py
+++ rest_framework_jwt/utils.py
@@ -23,7 +23,7 @@ def jwt_get_secret_key(payload=None):
     """
     if api_settings.JWT_GET_USER_SECRET_KEY:
         User = get_user_model()  # noqa: N806
-        user = User.objects.get(pk=payload.get('user_id'))
+        user = User.objects.get(pk=payload.get("user_id"))
         key = str(api_settings.JWT_GET_USER_SECRET_KEY(user))
         return key
     return api_settings.JWT_SECRET_KEY
@@ -34,35 +34,33 @@ def jwt_payload_handler(user):
     username = get_username(user)
 
     warnings.warn(
-        'The following fields will be removed in the future: '
-        '`email` and `user_id`. ',
-        DeprecationWarning
+        "The following fields will be removed in the future: "
+        "`email` and `user_id`. ",
+        DeprecationWarning,
     )
 
     payload = {
-        'user_id': user.pk,
-        'username': username,
-        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
+        "user_id": user.pk,
+        "username": username,
+        "exp": datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA,
     }
-    if hasattr(user, 'email'):
-        payload['email'] = user.email
+    if hasattr(user, "email"):
+        payload["email"] = user.email
     if isinstance(user.pk, uuid.UUID):
-        payload['user_id'] = str(user.pk)
+        payload["user_id"] = str(user.pk)
 
     payload[username_field] = username
 
     # Include original issued at time for a brand new token,
     # to allow token refresh
     if api_settings.JWT_ALLOW_REFRESH:
-        payload['orig_iat'] = timegm(
-            datetime.utcnow().utctimetuple()
-        )
+        payload["orig_iat"] = timegm(datetime.utcnow().utctimetuple())
 
     if api_settings.JWT_AUDIENCE is not None:
-        payload['aud'] = api_settings.JWT_AUDIENCE
+        payload["aud"] = api_settings.JWT_AUDIENCE
 
     if api_settings.JWT_ISSUER is not None:
-        payload['iss'] = api_settings.JWT_ISSUER
+        payload["iss"] = api_settings.JWT_ISSUER
 
     return payload
 
@@ -72,33 +70,29 @@ def jwt_get_user_id_from_payload_handler(payload):
     Override this function if user_id is formatted differently in payload
     """
     warnings.warn(
-        'The following will be removed in the future. '
-        'Use `JWT_PAYLOAD_GET_USERNAME_HANDLER` instead.',
-        DeprecationWarning
+        "The following will be removed in the future. "
+        "Use `JWT_PAYLOAD_GET_USERNAME_HANDLER` instead.",
+        DeprecationWarning,
     )
 
-    return payload.get('user_id')
+    return payload.get("user_id")
 
 
 def jwt_get_username_from_payload_handler(payload):
     """
     Override this function if username is formatted differently in payload
     """
-    return payload.get('username')
+    return payload.get("username")
 
 
 def jwt_encode_handler(payload):
     key = api_settings.JWT_PRIVATE_KEY or jwt_get_secret_key(payload)
-    return jwt.encode(
-        payload,
-        key,
-        api_settings.JWT_ALGORITHM
-    ).decode('utf-8')
+    return jwt.encode(payload, key, api_settings.JWT_ALGORITHM)
 
 
 def jwt_decode_handler(token):
     options = {
-        'verify_exp': api_settings.JWT_VERIFY_EXPIRATION,
+        "verify_exp": api_settings.JWT_VERIFY_EXPIRATION,
     }
     # get user from token, BEFORE verification, to get user secret key
     unverified_payload = jwt.decode(token, None, False)
@@ -111,7 +105,7 @@ def jwt_decode_handler(token):
         leeway=api_settings.JWT_LEEWAY,
         audience=api_settings.JWT_AUDIENCE,
         issuer=api_settings.JWT_ISSUER,
-        algorithms=[api_settings.JWT_ALGORITHM]
+        algorithms=[api_settings.JWT_ALGORITHM],
     )
 
 
@@ -130,6 +124,4 @@ def jwt_response_payload_handler(token, user=None, request=None):
         }
 
     """
-    return {
-        'token': token
-    }
+    return {"token": token}
